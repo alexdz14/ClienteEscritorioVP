84ea21c1800f2e4149a15d56be82414742ac2915
alexdz14 <zS22016132@estudiantes.uv.mx>
2025-10-22 19:25:28 -0600

    fix

--- src/clienteescritoriovp/utilidad/Constantes.java
+++ src/clienteescritoriovp/utilidad/Constantes.java
@@ -5,7 +5,7 @@ public class Constantes {
 
     public static final int ERROR_MALFORMED_URL = 1001;
     public static final int ERROR_PETICION = 1002;
-    public static final String URL_WS = "http://localhost:8084/proyecto/api";
+    public static final String URL_WS = "http://localhost:8084/proyecto/api/";
     
     public static final String METODO_POST = "POST";
     public static final String CONTENT_FORM = "application/x-www-form-urlencoded";
